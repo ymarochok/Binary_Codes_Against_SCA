c864b5890362a989ad6862a345ffecebc65e9732
ymarochok <ymarochok@gmail.com>
2026-04-15 15:14:46 +0200

    New c network version with 6 neurons in hidden layer and values except 0

--- new_version/nn_in_c sym/network_config.h
+++ new_version/nn_in_c sym/network_config.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define NET_INPUTS 10
-#define NET_L1 5
+#define NET_L1 6
 #define NET_L2 1
 
 // ===== INPUT QUANTIZATION =====
@@ -13,29 +13,30 @@ static const int INPUT_ZERO_POINT = 0; // 0 for symmetric
 
 // ===== LAYER 1 =====
 static const int8_t L1_W[NET_L1][NET_INPUTS] = {
-    { 1, -6, 7, -7, 4, -2, 0, 1, -1, 1 },
-    { -2, -4, -5, -5, -7, -2, -4, 0, 0, 0 },
-    { -2, 5, -6, 2, -1, 0, 1, 3, -7, 6 },
-    { -1, 1, -3, 3, -4, 4, -2, 0, 7, -5 },
-    { -3, 2, -2, 2, 1, 7, 5, 6, 4, 3 }
+    { -2, 4, -7, 5, -2, -1, 2, 2, -7, 4 },
+    { -3, -7, -5, -2, -1, -3, 2, -1, 2, -1 },
+    { -2, 5, -1, 5, 3, 4, 7, 4, 3, -1 },
+    { 3, 3, -2, 3, 1, -7, -4, -7, -5, -5 },
+    { -1, -4, 4, -6, 7, -6, 4, -1, 1, -1 },
+    { 1, -1, -3, 4, -3, 4, -2, -2, 7, -5 }
 };
 
-static const int32_t L1_B[NET_L1] = { -3, 16, -5, -4, 19 };
+static const int32_t L1_B[NET_L1] = { -5, 6, 8, 18, -1, -3 };
 
 // Multiplier (M0) and shift (n) per output channel
-static const int32_t L1_M0[NET_L1] = { 1331150097, 2140821279, 1326745243, 1625594765, 1630292597 };
-static const int32_t L1_N[NET_L1] = { 4, 5, 4, 4, 5 };
+static const int32_t L1_M0[NET_L1] = { 1259918104, 1794699278, 1863060630, 1135936513, 1237108040, 1439804747 };
+static const int32_t L1_N[NET_L1] = { 4, 5, 5, 5, 4, 4 };
 
 static const int ACT1_SCALE_INV = 0; // optional, if needed
 
 // ===== LAYER 2 =====
 static const int8_t L2_W[NET_L2][NET_L1] = {
-    { 7, -2, 7, 7, -3 }
+    { 7, -4, -3, -3, 7, 6 }
 };
 
-static const int32_t L2_B[NET_L2] = { -4 };
+static const int32_t L2_B[NET_L2] = { -1 };
 
-static const int32_t L2_M0[NET_L2] = { 1166033511 };
+static const int32_t L2_M0[NET_L2] = { 1353645644 };
 static const int32_t L2_N[NET_L2] = { -8 };
 
 // Output dequantization scale (for converting logit to float probability)
--- new_version/nn_p_sym/nn_to_cont/network_config.h
+++ new_version/nn_p_sym/nn_to_cont/network_config.h
@@ -13,30 +13,30 @@ static const int INPUT_ZERO_POINT = 0; // 0 for symmetric
 
 // ===== LAYER 1 =====
 static const int8_t L1_W[NET_L1][NET_INPUTS] = {
-    { 1, -5, -1, -1, -3, -4, -6, -7, -5, -3 },
-    { -1, 3, -2, -3, 5, -7, 4, -3, -1, 1 },
-    { -1, 5, -7, 5, -5, 4, -4, 1, 1, -1 },
-    { 1, -2, 2, -3, 1, 2, -4, 3, -7, 4 },
-    { 1, -4, 6, -4, 1, 1, -3, -1, 7, -6 },
-    { 3, -7, 1, 7, -6, -1, 5, -6, -1, 1 }
+    { -2, 4, -7, 5, -2, -1, 2, 2, -7, 4 },
+    { -3, -7, -5, -2, -1, -3, 2, -1, 2, -1 },
+    { -2, 5, -1, 5, 3, 4, 7, 4, 3, -1 },
+    { 3, 3, -2, 3, 1, -7, -4, -7, -5, -5 },
+    { -1, -4, 4, -6, 7, -6, 4, -1, 1, -1 },
+    { 1, -1, -3, 4, -3, 4, -2, -2, 7, -5 }
 };
 
-static const int32_t L1_B[NET_L1] = { 19, -12, -14, -11, -13, -14 };
+static const int32_t L1_B[NET_L1] = { -5, 6, 8, 18, -1, -3 };
 
 // Multiplier (M0) and shift (n) per output channel
-static const int32_t L1_M0[NET_L1] = { 1891320555, 1395975740, 1179142213, 1618603681, 1149714227, 1953088547 };
-static const int32_t L1_N[NET_L1] = { 5, 4, 4, 4, 4, 5 };
+static const int32_t L1_M0[NET_L1] = { 1259918104, 1794699278, 1863060630, 1135936513, 1237108040, 1439804747 };
+static const int32_t L1_N[NET_L1] = { 4, 5, 5, 5, 4, 4 };
 
 static const int ACT1_SCALE_INV = 0; // optional, if needed
 
 // ===== LAYER 2 =====
 static const int8_t L2_W[NET_L2][NET_L1] = {
-    { -3, 6, 7, 5, 6, 6 }
+    { 7, -4, -3, -3, 7, 6 }
 };
 
-static const int32_t L2_B[NET_L2] = { -6 };
+static const int32_t L2_B[NET_L2] = { -1 };
 
-static const int32_t L2_M0[NET_L2] = { 1924724802 };
+static const int32_t L2_M0[NET_L2] = { 1353645644 };
 static const int32_t L2_N[NET_L2] = { -8 };
 
 // Output dequantization scale (for converting logit to float probability)
